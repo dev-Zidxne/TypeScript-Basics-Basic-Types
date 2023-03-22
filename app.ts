4ef35424121494dbef0286bb69624272193a180c
Zidane Innis <zidaneinnis@gmail.com>
2023-03-22 00:54:46 -0500

    Using types

--- app.ts
+++ app.ts
@@ -1,1 +1,10 @@
-console.log('Time to get started...');
+function add(n1: number, n2: number) {
+  return n1 + n2;
+}
+
+const number1 = 16;
+
+const number2 = 29;
+
+const result = add(number1, number2);
+console.log(result);
