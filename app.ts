cf32eed489c556493f9e9c0fd0d5bf82a205995c
Zidane Innis <zidaneinnis@gmail.com>
2023-03-22 09:39:13 -0500

    Working with Numbers Strings Booleans

--- app.ts
+++ app.ts
@@ -1,7 +1,13 @@
-function add(n1: number, n2: number) {
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
   //   if (typeof n1 !== "number" || typeof n2 !== "number") {
   //     throw new Error("Invalid input value");
   //   }
+  const result = n1 + n2;
+  if (showResult) {
+    console.log(phrase + result);
+  } else {
+    return n1 + n2;
+  }
 
   return n1 + n2;
 }
@@ -9,6 +15,7 @@ function add(n1: number, n2: number) {
 const number1 = 16;
 
 const number2 = 29;
+const printResult = true;
+const resultPhrase = "Result is: ";
 
-const result = add(number1, number2);
-console.log(result);
+const result = add(number1, number2, printResult, resultPhrase);
