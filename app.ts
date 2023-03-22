10211bf476a4d83ce260b18b2c620f36c58bcd89
Zidane Innis <zidaneinnis@gmail.com>
2023-03-22 11:08:27 -0500

    Working with Tuples

--- app.ts
+++ app.ts
@@ -2,11 +2,22 @@
 //   name: string;
 //   age: number;
 // } = {
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: "Zidane",
   age: 21,
   hobbies: ["Sports", "Reading"],
+  role: [2, "author"],
 };
+
+// person.role.push("admin");
+// person.role[1] = 10;
+
+// person.role = [0, "admin", "user"];
 
 let favoriteActivities: string[];
 favoriteActivities = ["Sports"];
