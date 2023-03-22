eac26b192e7ae17832eba3d3c9dbea5d58e5c36b
Zidane Innis <zidaneinnis@gmail.com>
2023-03-22 11:32:28 -0500

    Working with enums

--- app.ts
+++ app.ts
@@ -2,16 +2,33 @@
 //   name: string;
 //   age: number;
 // } = {
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "Zidane",
+//   age: 21,
+//   hobbies: ["Sports", "Reading"],
+//   role: [2, "author"],
+// };
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
+const person = {
   name: "Zidane",
   age: 21,
   hobbies: ["Sports", "Reading"],
-  role: [2, "author"],
+  role: Role.ADMIN,
 };
 
 // person.role.push("admin");
@@ -27,4 +44,8 @@ console.log(person.name);
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
   //   console.log(hobby.map()); // ERROR
+}
+
+if (person.role === Role.ADMIN) {
+  console.log("Is admin");
 }
