1683c4b808bb42890c119591abfd922470719f99
Zidane Innis <zidaneinnis@gmail.com>
2023-03-22 09:56:22 -0500

    Object Types

--- app.ts
+++ app.ts
@@ -1,22 +1,10 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  //   if (typeof n1 !== "number" || typeof n2 !== "number") {
-  //     throw new Error("Invalid input value");
-  //   }
-  const result = n1 + n2;
-  if (showResult) {
-    console.log(phrase + result);
-  } else {
-    return n1 + n2;
-  }
+const person: {
+  name: string;
+  age: number;
+} = {
+  // const person = {
+  name: "Zidane",
+  age: 21,
+};
 
-  return n1 + n2;
-}
-
-let number1;
-number1 = 16;
-
-const number2 = 29;
-const printResult = true;
-let resultPhrase = "Result is: ";
-
-const result = add(number1, number2, printResult, resultPhrase);
+console.log(person.name);
