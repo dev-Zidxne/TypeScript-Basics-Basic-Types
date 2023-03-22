6d5305082e8bfeada5ffe8a6161f2cce794a216d
Zidane Innis <zidaneinnis@gmail.com>
2023-03-22 10:59:40 -0500

    Array types

--- app.ts
+++ app.ts
@@ -1,10 +1,19 @@
-const person: {
-  name: string;
-  age: number;
-} = {
-  // const person = {
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+const person = {
   name: "Zidane",
   age: 21,
+  hobbies: ["Sports", "Reading"],
 };
 
+let favoriteActivities: string[];
+favoriteActivities = ["Sports"];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+  //   console.log(hobby.map()); // ERROR
+}
