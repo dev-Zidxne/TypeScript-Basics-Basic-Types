21590d49e596883912b641e3e63aa5b06e027f31
Zidane Innis <zidaneinnis@gmail.com>
2023-03-22 13:27:57 -0500

    Function Return Types void

--- app.ts
+++ app.ts
@@ -1,29 +1,9 @@
-function combine(
-  input1: number | string,
-  input2: number | string,
-  resultConversion: "as-number" | "as-text"
-) {
-  let result;
-  if (
-    (typeof input1 === "number" && typeof input2 === "number") ||
-    resultConversion === "as-number"
-  ) {
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-  return result;
-  //   if (resultConversion === "as-number") {
-  //     return +result;
-  //   } else {
-  //     return result.toString();
-  //   }
+function add(n1: number, n2: number) {
+  return n1 + n2;
 }
-const combinedAges = combine(30, 26, "as-number");
-console.log(combinedAges);
 
-const combinedStringAges = combine("30", "26", "as-number");
-console.log(combinedStringAges);
+function printResult(num: number): void {
+  console.log("Result: " + num);
+}
 
-const combinednNames = combine("Max", "anna", "as-text");
-console.log(combinednNames);
+printResult(add(5, 12));
